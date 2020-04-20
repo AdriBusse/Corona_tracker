2da77db8003e2eb9832f73eedc1b3c03d38a3737
Adrian Busse <adri.busse@gmail.com>
2020-04-20 14:54:31 +0700

    google analytics

---
+++ build/precache-manifest.bfa3536b26bcb26eca1176d47aa92fba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fda60be0909a250bffd124b0949bd937",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73871c900b7db1ca00f2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7aaa3d7a684883bfa6a9",
+    "url": "/static/js/2.59235af1.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.59235af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73871c900b7db1ca00f2",
+    "url": "/static/js/main.05e830e1.chunk.js"
+  },
+  {
+    "revision": "8d552397e8e842fab456",
+    "url": "/static/js/runtime-main.35871c95.js"
+  }
+]);
